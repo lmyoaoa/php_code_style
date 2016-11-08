0ca4f997ddcdc8271b971c4d9900348e5d70948c
lmyoaoa <lmyoaoa@gmail.com>
2016-11-08 16:04:53 +0800

    新增一个文件

---
+++ PHP编码规范.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @desc 一个文件一个类，注释必须都写，本规范以一个类来作为范例
+ *
+ * @author 作者
+ * @since 2016-11-08
+ */
+namespace codestyle;
+
+use db\mysql;
+
+// 类名必须以大驼峰式命名
+// 类名之后必须跟空格
+class CodeStyle {
+}
+
+// 文件结尾不允许使用php结束符? >，防止出现多余的空格
